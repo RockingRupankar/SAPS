601e53a436daf7402ff8f66557395273847df9d2
RockingRupankar <141851811+RockingRupankar@users.noreply.github.com>
2023-09-21 19:32:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,171 @@
+
+
+var PS = document.querySelector(".PS-1");
+PS.addEventListener('click', e=> {
+document.querySelector(".PS-1").classList.toggle("toggle");
+document.getElementsByClassName("PS-1").addEventListener('clicl')
+
+ }); 
+
+var PS = document.querySelector(".PS-2");
+PS.addEventListener('click', e=> {
+ document.querySelector(".PS-2").classList.toggle("toggle");
+ 
+    
+}); 
+
+var PS = document.querySelector(".PS-3");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-3").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-4");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-4").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-5");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-5").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-6");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-6").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-7");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-7").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-8");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-8").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-9");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-9").classList.toggle("toggle");
+
+});
+
+
+var PS = document.querySelector(".PS-10");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-10").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-11");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-11").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-12");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-12").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-13");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-13").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-14");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-14").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-15");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-15").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-16");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-16").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-17");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-17").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-18");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-18").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-19");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-19").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-20");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-20").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-21");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-21").classList.toggle("toggle");
+
+});
+
+var PS = document.querySelector(".PS-22");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-22").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-23");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-23").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-24");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-24").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-25");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-25").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-26");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-26").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-27");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-27").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-28");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-28").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-29");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-29").classList.toggle("toggle");
+
+});
+var PS = document.querySelector(".PS-30");
+PS.addEventListener('click', e=> {
+    document.querySelector(".PS-30").classList.toggle("toggle");
+
+});
+
+
+
+
+
+
